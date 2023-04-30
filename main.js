fb6c84ee782e5ee214eafc512cb0d9827cfb3f22
Serhii Ihnatenko <serek.ua@gmail.com>
2023-04-30 20:28:19 +0300

    Added drawing of a winning line.  Styles for this line. Added short descriptions of functions.

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-let fieldOfPlay = document.getElementById('field');
-let reset = document.querySelector('.reset');
-let cellArr = fieldOfPlay.querySelectorAll('.cell');
-let informationContainer = document.querySelector('.information-line');
+// Initial data and flags
+const fieldOfPlay = document.getElementById('field');
+const reset = document.querySelector('.reset');
+const cellArr = fieldOfPlay.querySelectorAll('.cell');
+const informationContainer = document.querySelector('.information-line');
 let arrX = [];
 let arrO = [];
-let counter = 0;
+let moves = 0;
 let gamer = 1;
+let linePosition = 1;
 let courentArr = arrX;
 
+//Positions of winning combinations
 const winCombination = {
 	1: [11, 12, 13],
 	2: [21, 22, 23],
@@ -19,49 +22,77 @@ const winCombination = {
 	8: [13, 22, 31],
 }
 
-function winDetector(arr, winCobinationArr) {
-	let win = winCobinationArr.every(elem => {
-		// console.log('arr:', arr)
-		// console.log('elem:', elem)
-		let x = arr.includes(elem);
-		// console.log(x);
-		return x;
-	});
-	return win;
-};
+//Checkbox blocking. launching the player's turn function.
+fieldOfPlay.addEventListener("click", (e) => {
+	if (e.target.type == 'checkbox') {
+		e.target.setAttribute('disabled', 'disabled');
+		let element = e.target;
+		playersMove(element);
+	}
+})
 
+//formation of the player array. Checking the array for winning combinations.
+function playersMove(element) {
+
+	let id = element.getAttribute('id');
+	id = Number(id);
+
+	addClassName(element);
+
+	courentArr.push(id);
+
+	detector(courentArr, winCombination);
+}
+
+//Adding a class to display the desired image.
+function addClassName(elem) {
+	if (gamer == 1) {
+		elem.classList.add('cross');
+	};
+
+	if (gamer == 2) {
+		elem.classList.add('circle');
+	}
+}
+
+//Runs a function that checks for one of the 8 winning combinations.
+//Saving the number of the winning combination in linePosition.
 function detector(arr, obj) {
-	// console.log('start detector');
-	counter++;
+	moves++;
 
 	for (const key in obj) {
 		if (obj.hasOwnProperty.call(obj, key)) {
 			const element = obj[key];
 
 			let result = winDetector(arr, element);
-			// console.log(x);
 			if (result) {
-
+				linePosition = key;
 				return endGame(result);
 			};
 		}
-	}
-	// console.log('not win combination');
-	chekingCounter(counter);
+	};
+
+	checkingMoves(moves);
 	changeGamer();
 };
 
-function chekingCounter(counter) {
-	// console.log('counter = ', counter);
-	if (counter === 9) {
-		return endGame(counter);
+//Checking if a winning combination exists in the player's array. return true or false.
+function winDetector(arr, winCombinationArr) {
+	let win = winCombinationArr.every(elem => arr.includes(elem));
+	return win;
+};
+
+//Check for a limit on the number of steps.
+function checkingMoves(moves) {
+	if (moves === 9) {
+		return endGame(moves);
 	} else {
 		console.log('continue');
 	}
 }
 
+// Changing the player and the value of the check array.
 function changeGamer() {
-	// console.log('changin gamer');
 	if (gamer == 1) {
 		gamer = 2;
 		courentArr = arrO;
@@ -70,104 +101,109 @@ function changeGamer() {
 		courentArr = arrX;
 	}
 	changingPlayerInformation(gamer);
-	// console.log(`next gamer ${gamer}`)
-	// console.log(`---------------`)
 }
 
+//Displaying information about which player's turn.
+function changingPlayerInformation(gamer) {
+	if (gamer === 1) {
+		informationContainer.classList.remove('circle');
+		informationContainer.classList.add('cross');
+	}
+	if (gamer === 2) {
+		informationContainer.classList.remove('cross');
+		informationContainer.classList.add('circle');
+	}
+}
+
+//Formatting text for the end of the game.
 function endGame(flag) {
 	let informeText = ''
 	if (typeof (flag) == 'number' && flag === 9) {
-		// console.log('no more moves. draw.');
 		informeText = `No more moves.
 		Game restart`;
 	} else {
-		// console.log('win');
 		let nameWiner = gamer == 1 ? 'Cross' : "Circle";
 		informeText = `Winner ${nameWiner}.
 		Game restart`;
 		disabledCell();
-	};
-	// resetGame();
-	// setTimeout(() => alert(informeText), 1500)
-	setTimeout(() => resetGame(informeText), 1500);
-}
-
-function resetGame(informeText = 'Game restart') {
-	// console.log(informeText);
-	// alert(informeText);
-	finallMessage(informeText);
-	arrO.length = 0;
-	arrX.length = 0;
-	counter = 1;
-	courentArr = arrX;
-	gamer = 1;
-	clearCell();
-	changingPlayerInformation(gamer);
-
-	// console.log('arrX', arrX)
-	// console.log('arrO', arrO)
-	// console.log('counter', counter)
-	// console.log('gamer', gamer)
-	// console.log('courentArr', courentArr)
-}
-
-fieldOfPlay.addEventListener("click", (e) => {
-	if (e.target.type == 'checkbox') {
-		// console.log('click');
-		// console.log(e.target);
-		// e.target.classList.add('cross');
-		e.target.setAttribute('disabled', 'disabled');
-		let element = e.target;
-		playersMove(element);
-	}
-})
-
-function playersMove(element) {
-	// console.log('playersMove');
-
-	let id = element.getAttribute('id');
-	id = Number(id);
-	// console.log('click on:', id);
-
-	addClassName(element);
-
-	courentArr.push(id);
-	// console.log('Push id', id);
-
-	detector(courentArr, winCombination);
-}
-
-function addClassName(elem) {
-	if (gamer == 1) {
-		elem.classList.add('cross');
-		// console.log('add class name "Cross"')
+		setTimeout(() => drawWinnerLine(), 1000);
 	};
 
-	if (gamer == 2) {
-		elem.classList.add('circle');
-		// console.log('Add class name "Circle"');
-	}
+	setTimeout(() => resetGame(informeText), 3000);
 }
 
+//Blocking free cells. Subject to winning and the availability of free cells.
 function disabledCell() {
 	for (const cell of cellArr) {
-		// console.log(cell);
 		if (!cell.hasAttribute('disabled')) {
 			cell.setAttribute('disabled', 'disabled');
 		}
 	};
 }
 
-function clearCell() {
-	for (const cell of cellArr) {
-		cell.removeAttribute('disabled');
-		cell.checked = false;
-		cell.classList.remove('cross', 'circle');
+//The function shapes coordinates for displaying a winning line depending on the winning combination.
+function drawWinnerLine() {
+	switch (linePosition) {
+		case '1':
+			setWinnerLineStyle(14.5, 0, 0);
+			break;
+		case '2':
+			setWinnerLineStyle(49, 0, 0);
+			break;
+		case '3':
+			setWinnerLineStyle(83.5, 0, 0);
+			break;
+		case '4':
+			setWinnerLineStyle(-1, 15.2, 90);
+			break;
+		case '5':
+			setWinnerLineStyle(-1, 50, 90);
+			break;
+		case '6':
+			setWinnerLineStyle(-1, 84.6, 90);
+			break;
+		case '7':
+			setWinnerLineStyle(0, 1, 45);
+			break;
+		case '8':
+			setWinnerLineStyle(0, 99, 135);
+			break;
+		default:
+			console.log('non-existent value');
+			break;
 	};
+
+	fieldOfPlay.classList.add('line');
+	setTimeout(() => fieldOfPlay.classList.add('line__show'), 500);
 }
 
+//the function sets the styles for the winning line.
+function setWinnerLineStyle(top, left, rotate) {
+
+	fieldOfPlay.style.setProperty('--positionTop', `${top}%`);
+	fieldOfPlay.style.setProperty('--positionLeft', `${left}%`);
+	fieldOfPlay.style.setProperty('--rotateValue', `${rotate}deg`);
+	if (rotate == 45 || rotate == 135) {
+		let width = 100 * Math.sqrt(2);
+		fieldOfPlay.style.setProperty('--width', `${width}%`);
+	}
+}
+
+//Reset all values ​​to initial.
+function resetGame(informeText = 'Game restart') {
+	finallMessage(informeText);
+	arrO.length = 0;
+	arrX.length = 0;
+	moves = 0;
+	courentArr = arrX;
+	gamer = 1;
+	deletedWinnerLine();
+	clearCell();
+	changingPlayerInformation(gamer);
+}
+
+//Displaying the result of the game.
 function finallMessage(msg) {
-	// console.log('finallMessage');
 	let msgContainer = document.createElement('div');
 	msgContainer.classList.add('msg');
 	msgContainer.classList.add('show');
@@ -180,17 +216,24 @@ function finallMessage(msg) {
 	}, 2500)
 }
 
-function changingPlayerInformation(gamer) {
-	if (gamer === 1) {
-		informationContainer.classList.remove('circle');
-		informationContainer.classList.add('cross');
-	}
-	if (gamer === 2) {
-		informationContainer.classList.remove('cross');
-		informationContainer.classList.add('circle');
-	}
+//Removing the winning line and clearing styles.
+function deletedWinnerLine() {
+	fieldOfPlay.classList.remove('line__show');
+	fieldOfPlay.classList.remove('line');
+	fieldOfPlay.removeAttribute('style');
 }
 
+//Cell cleaning. Their styles. Reset the selected state.
+function clearCell() {
+	for (const cell of cellArr) {
+		cell.removeAttribute('disabled');
+		cell.checked = false;
+		cell.classList.remove('cross', 'circle');
+	};
+}
+
+//Output of initial information about which player it is now to move.
 changingPlayerInformation(gamer);
 
+//Reload game on button click.
 reset.addEventListener('click', () => resetGame());
